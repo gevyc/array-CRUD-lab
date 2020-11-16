853f28a73b4c1a6b5111859c22b95c63235fad29
Gevy Cunanan <gevycunanan@icloud.com>
2020-11-15 20:25:59 -0500

    commit tree b4ead3b773236a7d00da9db147e2d2ca79cc6102

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  chicagobulls = ["DRose", "MJ", "Pippen", "Rodman"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  chicagobulls = ["DRose", "MJ", "Pippen", "Rodman"]
+  chicagobulls << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    chicagobulls = ["DRose", "MJ", "Pippen", "Rodman"]
+    chicagobulls.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    chicagobulls = ["DRose", "MJ", "Pippen", "Rodman", "arrays!"]
+    arrays_chicagobulls = chicagobulls.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    chicagobulls = ["wow", "DRose", "MJ", "Pippen", "Rodman", "arrays!"]
+    wow_chicagobulls = chicagobulls.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    chicagobulls = ["wow", "DRose", "am", "MJ", "Pippen", "Rodman", "arrays!"]
+    chicagobulls[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    chicagobulls = ["wow", "DRose", "am", "MJ", "Pippen", "Rodman", "arrays!"]
+    chicagobulls.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+    chicagobulls = ["wow", "DRose", "am", "MJ", "Pippen", "Rodman", "arrays!"]
+    chicagobulls.last
 end
